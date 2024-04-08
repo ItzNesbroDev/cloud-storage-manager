8bc05b538cd8bc1f93e5f7d97275509500b11fb5
Nesbeer <asnesbeer3@gmail.com>
2024-04-08 09:05:11 +0000

    fix: changed api

--- src/api.js
+++ src/api.js
@@ -1,2 +1,1 @@
-// export const API_URL = `http://127.0.0.1:3000`;
-export const API_URL = `https://3000-itznesbrode-cloudstorag-tr37zxmuz1g.ws-us110.gitpod.io`;
+export const API_URL = `http://127.0.0.1:3000`;
